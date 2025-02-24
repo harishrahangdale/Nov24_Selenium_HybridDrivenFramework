0cd0aa51c281dd51b62376b99dddcc1b8c26b33b
Harish Rahangdale <hsr.29978@gmail.com>
2025-02-24 20:21:51 +0530

    Updated TestConfigs.java file

--- src/base/TestConfigs.java
+++ src/base/TestConfigs.java
@@ -2,8 +2,8 @@ package base;
 
 public class TestConfigs {
 	
-	public static final String VALID_USERNAME = "hsr.29978@gmail.com";
-    public static final String VALID_PASSWORD = "Pass@123";
+	public static final String VALID_USERNAME = "your-username-here";
+    public static final String VALID_PASSWORD = "your-password-here";
     public static final String BASE_URL = "https://staging.app.hirecorrecto.com";
     
     public static final String EXPECTED_DASHBOARD_URL = "https://staging.app.hirecorrecto.com/dashboard";
