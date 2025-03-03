2c409f8e4430c6f1e040a85de71829e65f3937fc
Harish Rahangdale <hsr.29978@gmail.com>
2025-03-03 21:53:04 +0530

    added teardown method in DashboardTest.java

--- src/testscripts/DashboardTest.java
+++ src/testscripts/DashboardTest.java
@@ -3,6 +3,7 @@ package testscripts;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -66,4 +67,10 @@ public class DashboardTest {
 		Assert.assertTrue(dashboardPage.isCreateAssessmentButtonLabelVisible(),
 				"Create Assessement button was not displayed");
 	}
+
+	@AfterMethod
+	public void tearDown() {
+		ControlActions.closeBrowser();
+	}
+
 }
